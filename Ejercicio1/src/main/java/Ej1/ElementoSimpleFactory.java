642aad4896d1d5e8e1224448ca0cd8485a0c8416
franlopezsoto <flopesot@myuax.com>
2024-04-15 15:10:14 +0200

    Excepciones

--- Ejercicio1/src/main/java/Ej1/ElementoSimpleFactory.java
+++ Ejercicio1/src/main/java/Ej1/ElementoSimpleFactory.java
@@ -3,7 +3,14 @@ package Ej1;
 public class ElementoSimpleFactory implements ElementoFactory {
     @Override
     public Elemento crearElemento(double precio, int codigo) {
+        if (precio < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo");
+        }
+        if (codigo < 0) {
+            throw new IllegalArgumentException("El código no puede ser negativo");
+        }
         return new ElementoSimple(precio, codigo);
     }
 }
+
 
--- Ejercicio1/src/main/java/Ej1/KitFactory.java
+++ Ejercicio1/src/main/java/Ej1/KitFactory.java
@@ -6,4 +6,3 @@ public class KitFactory implements ElementoFactory {
             return new Kit(codigo);
         }
     }
-}
